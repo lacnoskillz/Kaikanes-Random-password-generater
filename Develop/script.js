a690b4316370a153cd38244c6fb92efe8bfcbcb8
lacnoskillz <kaikane3457@hotmail.com>
2022-10-26 23:15:38 -0500

    made assets folder and moved style.css in it. completed readme file with screen shot and link to deployed site

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,75 @@
 // Assignment code here
-
-
+var passwordlength = 0;
+var lowercaseletters;
+var uppercaseletters;
+var numbers;
+var specialchar;
+var array = [];
+var x = 0;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  choice = confirm("want to make a password?");
+  if (choice != true){
+    return;
+  } 
+  choice = prompt("what length?");
+  if((choice >= 8) && (choice < 128)){
+    console.log(choice);
+    passwordlength = choice;
+    console.log(passwordlength);
+  } else {
+    console.log("please enter a number 8 - 128");
+    return
+  }
+  choice = confirm("want lowercase letters?");
+    lowercaseletters = choice;
+    console.log(lowercaseletters);
+  choice = confirm("want uppercase letters?");
+    uppercaseletters = choice;
+    console.log(uppercaseletters);
+  choice = confirm("want numbers?");
+    numbers = choice;
+    console.log(numbers)
+  choice = confirm("want special characters?");
+    specialchar= choice;
+    console.log(specialchar);
+  if((lowercaseletters == false) && (uppercaseletters == false) && (numbers == false) && (specialchar == false)){
+    alert("please confirm whether or not to include at LEAST ONE of the following lowercase, uppercase, numeric, special characters ");
+    alert("try again");
+    return
+  } else {
+    array.length = passwordlength;
+    console.log("array length",array);
+  }
+    if( lowercaseletters == true) {
+      x= x+1
+    } if (uppercaseletters == true) {
+      x= x+1;
+    } if (numbers == true) {
+      x= x+1;
+    } if (specialchar == true) {
+      x= x+1;
+    }
+    console.log("number of true statements",x);
+
+   
+  
+  
+
+
+
+
+
+
+
+
+  //var password = generatePassword()
+    
+  
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Password Generator Starter Code
+# Random-password-generater
+***
+A site that makes a random password according to your specifications. such as including or not including lowercase, uppercase, numeric, and/or special characters.
+## Usage
+***
+To create a strong password that provides greater security.
+### Technologys
+***
+* www.google.com
+* www.w3schools.com
+* Github.com
+* Slack
+* Zoom
+#### Screen shot
+***
+![image](/friendly-parakeet/Develop/assets/images/Screenshot.png)
+##### Link
+***
+click for deployed app
+[click me](https://lacnoskillz.github.io/Random-password-generater/)
